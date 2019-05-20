af16354386bcf17842c6b84109d03eb2ab1aa61a
vucat2000 <cnikolai@earthlink.net>
2019-05-20 06:36:26 -0400

    verticle sliding tabs works

---
+++ app/src/main/java/com/nikolai/moodtracker/controller/FragmentOne.java
@@ -0,0 +1,55 @@
+package com.nikolai.moodtracker.controller;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.nikolai.moodtracker.R;
+
+public class FragmentOne extends Fragment {
+
+    private static final String MY_NUM_KEY = "num";
+    private static final String MY_COLOR_KEY = "color";
+    public static final String SMILEY_TYPE = "smiley_type";
+
+    private int mNum;
+    private int mColor;
+    private int mImageName;
+
+    // You can modify the parameters to pass in whatever you want
+    static FragmentOne newInstance(int num, int color, int smiley_type) {
+        FragmentOne f = new FragmentOne();
+        Bundle args = new Bundle();
+        args.putInt(MY_NUM_KEY, num);
+        args.putInt(MY_COLOR_KEY, color);
+        args.putInt(SMILEY_TYPE, smiley_type);
+        f.setArguments(args);
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mNum = getArguments() != null ? getArguments().getInt(MY_NUM_KEY) : 0;
+        mColor = getArguments() != null ? getArguments().getInt(MY_COLOR_KEY) : Color.BLACK;
+        mImageName = getArguments() != null ? getArguments().getInt(SMILEY_TYPE) : R.drawable.ic_smiley_happy;
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_one, container, false);
+        //change the image and the background color of the screen as the screen is swiped
+        v.setBackgroundColor(mColor);
+        ImageView image = (ImageView) v.findViewById(R.id.smiley_image);
+        image.setImageResource(mImageName);
+        //TextView textView = v.findViewById(R.id.textview);
+        //textView.setText("Page " + mNum);
+        return v;
+    }
+}
--- app/src/main/java/com/nikolai/moodtracker/controller/MainActivity.java
+++ app/src/main/java/com/nikolai/moodtracker/controller/MainActivity.java
@@ -1,30 +1,33 @@
 package com.nikolai.moodtracker.controller;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 
 import com.nikolai.moodtracker.R;
-import com.nikolai.moodtracker.view.SlidingTabsBasicFragment;
 
-public class MainActivity extends ActivityBase {
+public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
 
     // Whether the Log Fragment is currently shown
-    private boolean mLogShown;
+    //private boolean mLogShown;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        if (savedInstanceState == null) {
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            SlidingTabsBasicFragment fragment = new SlidingTabsBasicFragment();
-            transaction.replace(R.id.content_fragment, fragment);
-            transaction.commit();
-        }
-    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        if (savedInstanceState == null) {
+//            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//            SlidingTabsBasicFragment fragment = new SlidingTabsBasicFragment();
+//            transaction.replace(R.id.content_fragment, fragment);
+//            transaction.commit();
+//        }
+//    }
 
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
@@ -58,7 +61,9 @@ public class MainActivity extends ActivityBase {
 //        return super.onOptionsItemSelected(item);
 //    }
 
-    /** Create a chain of targets that will receive log data */
+    /**
+     * Create a chain of targets that will receive log data
+     */
 //    @Override
 //    public void initializeLogging() {
 //        // Wraps Android's native log framework.
@@ -77,4 +82,57 @@ public class MainActivity extends ActivityBase {
 //
 //        Log.i(TAG, "Ready");
 //    }
+
+    static final int NUMBER_OF_PAGES = 5;
+
+    MyAdapter mAdapter;
+    VerticalViewPager mPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAdapter = new MyAdapter(getSupportFragmentManager());
+        mPager = findViewById(R.id.viewpager);
+        mPager.setAdapter(mAdapter);
+    }
+
+    public class MyAdapter extends FragmentPagerAdapter {
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return NUMBER_OF_PAGES;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            switch (position) {
+                case 0:
+                    //ContextCompat.getColor(MainActivity.this, R.color.colorYellow)
+                    //Color.parseColor("#00ff00")
+                    return FragmentOne.newInstance(0, ContextCompat.getColor(MainActivity.this, R.color.colorYellow),R.drawable.ic_smiley_super_happy);
+                case 1:
+                    // return a different Fragment class here
+                    // if you want want a completely different layout
+                    return FragmentOne.newInstance(1, ContextCompat.getColor(MainActivity.this, R.color.colorGreen),R.drawable.ic_smiley_happy);
+                    //return null;
+                case 2:
+                    return FragmentOne.newInstance(2, ContextCompat.getColor(MainActivity.this, R.color.colorBlue),R.drawable.ic_smiley_normal);
+                    //return null;
+                case 3:
+                    return FragmentOne.newInstance(3, ContextCompat.getColor(MainActivity.this, R.color.colorGrey),R.drawable.ic_smiley_disappointed);
+                    //return null;
+                case 4:
+                    return FragmentOne.newInstance(4, ContextCompat.getColor(MainActivity.this, R.color.colorRed),R.drawable.ic_smiley_sad);
+                    //return null;
+                default:
+                    return null;
+            }
+        }
+    }
 }
